211fe5d15c325241973dc72ffbbb438da9431cd6
red-soull <rijaiaola@gmail.com>
2021-11-09 06:09:25 +0300

    fix: fix file info output

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,9 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 
-// const dirName = 'secret-folder';
-const dirPath = path.join(__dirname, 'secret-folder');
+const dirName = 'secret-folder';
+const dirPath = path.join(__dirname, dirName);
 
+
+function getFileInfo(file) {
+    let fileExt = path.extname(file);
+    let fileExtName = path.extname(file).slice(1);
+    let fileName = path.basename(file, fileExt);
+
+    fs.stat(path.join(dirPath, file), (error, data) => {
+        const fileSize = Math.round(data['size'] / 1024).toString() + 'kb';
+
+       console.log(`${fileName} - ${fileExtName} - ${fileSize}`);
+    })
+    
+}
 
 fs.promises.readdir(dirPath, {withFileTypes: true}).then(
     files => {
@@ -13,19 +26,5 @@ fs.promises.readdir(dirPath, {withFileTypes: true}).then(
             }
         }
     })
-
-function getFileInfo(file) {
-    
-    let fileExtName = path.extname(file);
-    let fileName = path.basename(file, fileExtName);
-    let filePath = path.join(dirPath, file);
-
-    fs.stat(filePath, (error, data) => {
-        const fileSize = Math.ceil(data['size'] / 1024);
-
-       console.log(`${fileName}` - `${fileExtName}` - `${fileSize}`);
-    })
-    
-}
 
 
